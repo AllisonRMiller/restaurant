ad371a3cf77354ffd8d1c7591763d18cad14573f
AllisonRMiller <60373159+AllisonRMiller@users.noreply.github.com>
2020-04-03 08:31:50 -0400

    Updates

---
+++ precache-manifest.c46b0f1e42b7bed8c9a642bc66eb2ce7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338bc18653a220751ee45130415f5834",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "318facd0b996921e12ed",
+    "url": "/restaurant/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "697e8004bd7a801827a0",
+    "url": "/restaurant/static/css/main.7fae8be1.chunk.css"
+  },
+  {
+    "revision": "318facd0b996921e12ed",
+    "url": "/restaurant/static/js/2.31827116.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant/static/js/2.31827116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "697e8004bd7a801827a0",
+    "url": "/restaurant/static/js/main.2d06240d.chunk.js"
+  },
+  {
+    "revision": "c4cd640d4c6a0e21698a",
+    "url": "/restaurant/static/js/runtime-main.27189726.js"
+  },
+  {
+    "revision": "aae4d503f0accbe1c53fcbb3e1ba51a2",
+    "url": "/restaurant/static/media/jez-timms-IlpXSjNSNRI-unsplash.aae4d503.jpg"
+  },
+  {
+    "revision": "42da0a6da75a042c9b1a70dcf17326f2",
+    "url": "/restaurant/static/media/patrick-hendry-qSmomJCEUiA-unsplash.42da0a6d.jpg"
+  }
+]);
