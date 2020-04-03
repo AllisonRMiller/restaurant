4f1cc1bb9961d01fd8435bdd99fa12e846ad5ae2
AllisonRMiller <60373159+AllisonRMiller@users.noreply.github.com>
2020-04-03 08:27:09 -0400

    More minor tweaks
    More minor readability changes, preparing to deploy

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,31 @@
 import React from 'react';
 
 
-function Navbar (){
-        return (
-            <div>
-            <nav className="navbar navbar-expand-md fixed-top navbar-dark bg-dark text-light" id="topnav">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-              <ul className="navbar-nav mx-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="#Menu">Menu <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#Hours">Hours & Location</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="Contact">Contact Us</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          </div>
-        )
-    
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-md fixed-top navbar-dark bg-dark text-light" id="topnav">
+        <button className="navbar-toggler" type="button" data-toggle="collapse"
+          data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#Menu">Menu <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#Hours">Hours & Location</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="Contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+
 }
 
 export default Navbar
